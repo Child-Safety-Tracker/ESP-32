d59650628dcd4864d7b091c2add0100de4d32c66
cielaton <vuwin24680@gmail.com>
2024-05-22 09:54:11 +0700

    working flawlessly

--- GPS-device-esp32/GPS-device-esp32.ino
+++ GPS-device-esp32/GPS-device-esp32.ino
@@ -4,46 +4,60 @@
 
 HardwareSerial SerialPort(2);
 
-const char* ssid = "Giangvien";
-const char* password = "dhbk@2024";
+const char* ssid = "ai cho";
+const char* password = "123456788";
 
-String serverName = "http://35.209.25.56:80/";
-String locationPath = serverName + "location";
+char* serverName = "http://35.209.25.56:80/location";
 
 String receivedLocation = "";
-String longitude = "";
-String latitude = "";
-int accuracy = 0;
+String modifiedReceivedLocation = "";
+
 
 unsigned long lastTime = 0;
-unsigned long timerDelay = 5000;
+unsigned long timerDelay = 3000;
 
 void setup() {
-  WiFi.begin(ssid, password);
   Serial.begin(115200);
   SerialPort.begin(115200, SERIAL_8N1, 16, 17);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    Serial.println(WiFi.localIP());
+  }
 }
 
 void loop() {
-  
-  if (SerialPort.available() > 0){
+
+  if (SerialPort.available() > 0) {
     receivedLocation = SerialPort.readString();
-    //SerialPort.println(receivedLocation);
-  }
-    
-  if ((millis() - lastTime) > timerDelay) {
+    for (int i = 0; i < receivedLocation.length(); i++) {
+      if (receivedLocation[i] == '\0')
+        break;
+      modifiedReceivedLocation += receivedLocation[i];
+    }
+    // SerialPort.println(receivedLocation);
+    // }
+
+    // if ((millis() - lastTime) > timerDelay) {
+
+
     if (WiFi.status() == WL_CONNECTED) {
+      Serial.println(modifiedReceivedLocation);
       WiFiClient client;
       HTTPClient http;
 
-      http.begin(client, locationPath);
+      http.begin(client, serverName);
 
       http.addHeader("Content-Type", "application/json");
-      int httpResponseCode = http.POST(receivedLocation);
-
+      int httpResponseCode = http.POST(modifiedReceivedLocation);
+      Serial.println(httpResponseCode);
       http.end();
     } else {
     }
+    // lastTime = millis();
   }
-  lastTime = millis();
+  if (receivedLocation.length() != 0)
+    modifiedReceivedLocation = "";
 }
